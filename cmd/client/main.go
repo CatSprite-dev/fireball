17fc014220490394786e1fe309fd1bd698f10516
CatSprite-dev <catspriteee@gmail.com>
2026-01-16 15:40:18 +0300

    Добавлен логотип

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	cfg, err := LoadConfig()
+	_, err := LoadConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-	GetPositionsInfo(&cfg)
+
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("front")))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on: http://localhost:%s/\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
